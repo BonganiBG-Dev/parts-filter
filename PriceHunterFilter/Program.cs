b4dd40cf10b47ef04d035c55c94646115fba2e09
Bonganibg <bongani.gumbo@yahoo.com>
2023-11-03 22:13:09 +0200

    implemented rabbitmq consumer

--- PriceHunterFilter/Program.cs
+++ PriceHunterFilter/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace PriceHunterFilter
-{
+﻿using PriceHunterFilter.Services;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+namespace PriceHunterFilter
+{    
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            RabbitMqService rabbitService = new RabbitMqService();
+
+            rabbitService.Consume((data) =>
+            {
+                Console.WriteLine(data);
+            });
+        }
+
+        private static bool TryDeserialize<T>(string jsonObject, out T dataObj)
+        {
+            bool assigned = false;
+
+            try
+            {
+                dataObj = JsonSerializer.Deserialize<T>(jsonObject);
+                assigned = true;
+            }
+            catch (Exception)
+            {
+                dataObj = default(T);
+                // Log the error
+            }
+
+            return assigned;
         }
     }
 }
